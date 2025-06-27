a06999fbc0302bf7894ab1ed3754a4168887f1d5
MBOUP Mouhamadou Lamine <laphisco@yahoo.fr>
2025-06-27 15:27:45 +0200

    Test two-way-binding for adding a wish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishList } from "../shared/wishList";
 import {NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf],
+  imports: [RouterOutlet, NgForOf, NgIf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,9 +18,14 @@ export class AppComponent {
     new WishList("Build a house", true),
     new WishList("Make a coffee"),
   ];
+  newWishText: string = "";
 
   checkOrUncheck(wish: WishList): void {
     wish.isCompleted = !wish.isCompleted;
   }
 
+  addWish() {
+    this.wishes.push(new WishList(this.newWishText));
+    this.newWishText = "";
+  }
 }
